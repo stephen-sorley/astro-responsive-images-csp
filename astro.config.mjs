b2e6c2c19e0a142b9c4260b60d7b4db48689f2da
Stephen Sorley <stephen.sorley@cccgainesville.org>
2026-05-07 19:09:23 -0400

    minimal repro for responsive images CSP issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  image: {
+    // Enable responsive images:
+    layout: 'constrained',
+    responsiveStyles: false,
+  },
+  security: {
+    csp: true,
+  },
+});
